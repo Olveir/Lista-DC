0e8fd01b6ed7655f21607c9c0f93fa53be090e33
Otávio <otavioolv@sempreceub.com>
2023-10-14 20:51:44 -0300

    correção de erros

--- listaDC.c
+++ listaDC.c
@@ -13,196 +13,261 @@ typedef struct Lista{
     struct No* fim;
 } lista;
 
-void cria_lista(lista* l, int tam){
-    //lista* p = (lista*) malloc(sizeof(lista));
+lista* cria_lista(){
+    lista *l = (lista *)malloc(sizeof(lista));
     l -> ini=NULL;
     l->fim=NULL;
     l->tam=0;
+    return l;
 }
 
-no* inicializa(void){
-    return NULL;
-}
-
-no* criar(no* l, int num){
+no* criar(lista* l, int num){
     no* p = (no*)malloc(sizeof(no));
     p->val = num;
     return p;
 }
 
-void libera(no* N){
-    no* p= N;
+no* libera(lista* l){
+    no* p = l -> ini;
     while (p != NULL) {
-        no* t = p->prox; /* guarda refer�ncia para o pr�ximo elemento*/
-        free(p); /* libera a mem�ria apontada por p */
-        p = t; /* faz p apontar para o pr�ximo */
+        no* t = p->prox; /* guarda referencia para o proximo elemento*/
+        free(p); /* libera a memoria apontada por p */
+        p = t; /* faz p apontar para o proximo */
     }
+    return p;
 }
 
-no* inserir_inicio(no* N, lista* l, int num, int tam){
-    no* novoNo = criar(N, num);
-
+lista* inserir_inicio(lista* l, int num){
+    no* novoNo = criar(l, num);
+    /*condicao para verificar se a memoria foi alocada*/
     if(novoNo){
-        novoNo->val = num;
-        novoNo->prox = l->ini;
-        novoNo->ant = l->fim;
-        l->ini = novoNo;
-        if(l->fim==NULL){
-            l->fim = novoNo;
-        } 
-        l->fim->prox= l->ini;
-        l->ini->ant= l->fim;
-        l->tam++;
-        return novoNo;
-    } else {
-        printf("Erro ao alocar memoria, tente novamente");
-        libera(N);
-        return N;
-    }
-}
-
-no* inserir_fim(no* N, lista* l, int num, int tam){
-    no* novoNo,* aux = criar(N, num);
-
-    if (novoNo){
-        novoNo->val=num;
-        /*condicao para verificar se é o primeiro elemento a ser inserido*/
+        /*condicao para verificar se e o primeiro elemento a se inserido*/
         if(l->ini==NULL){
-            l->ini=novoNo;
-            l->fim=novoNo;
-            l->fim->prox=l->ini;
-            l->ini->ant=l->fim;
-        } else{
-            /*aux = N;
-            auxL = l;
-            while (aux->prox&&auxL->fim)
-            {
-            aux = aux->prox;
-            auxL= auxL->fim;
-            }
-            aux->prox= novoNo;
-            novoNo->ant = aux;*/
-            l->fim->prox = novoNo;
-            l->ini->ant = l->fim;
+            novoNo->val=num;
+            l->ini = novoNo;
             l->fim = novoNo;
+            novoNo->ant = novoNo;
+            novoNo->prox = novoNo;
+        } else {
+            novoNo->val=num;
+            novoNo->ant = l->fim;
             novoNo->prox = l->ini;
+            l->fim->prox = novoNo;
+            l->ini->ant = novoNo;
+            l->ini = novoNo;
         }
+        printf("Numero adicionado com sucesso.\n");
         l->tam++;
-        return novoNo;
+        return l;
+
     } else {
+        /*condicao caso um erro ocorra erro na alocacao de memoria seguido com a funcao libera para liberar espaco na memoria*/
         printf("Erro ao alocar memoria, tente novamente");
-        libera(N);
-        return N;
+        libera(l);
+        return l;
     }
 }
 
-no* remover(no* N, lista* l, int num, int tam){
-    no* p;
-    p = N;
-    /*if(N->prox == NULL){
-        printf("lista vazia.");*/
-     if(N == NULL){
-        printf("lista vazia.");
-        return N;
+lista* inserir_fim(lista* l, int num){
+    no* novoNo = criar(l, num);
+    /*condicao para verificar se a memoria foi alocada*/
+    if (novoNo){
+        /*condicao para verificar se e o primeiro elemento a se inserido*/
+        if(l->ini==NULL){
+            novoNo->val=num;
+            l->ini = novoNo;
+            l->fim = novoNo;
+            novoNo->ant = novoNo;
+            novoNo->prox = novoNo;
+        } else {
+            novoNo->val=num;
+            novoNo->ant = l->fim;
+            novoNo->prox = l->ini;
+            l->fim->prox = novoNo;
+            l->ini->ant = novoNo;
+            l->fim = novoNo;
+        }
+        printf("Numero adicionado com sucesso.\n");
+        l->tam++;
+        return l;
+
+    } else {
+        /*condicao caso um erro ocorra erro na alocacao de memoria seguido com a funcao libera para liberar espaco na memoria*/
+        printf("Erro ao alocar memoria, tente novamente");
+        libera(l);
+        return l;
     }
-    while (p!=NULL&&p->val!= num)
+}
+
+lista* remover(lista* l){
+    no* aux = l->ini;
+    int remover = 0;
+
+    if (l->ini==NULL)
+    {
+        printf("A lista esta vazia.");
+        return l;
+    }
+
+    printf("\nInsira o numero que deseja remover: \n");
+    scanf("%d", &remover);
+    getchar();
+
+    do
+    {
+        /*condicao para verificar se o numero foi encontrado*/
+        if (aux->val == remover)
         {
-            p= p->prox;
-            if (p==NULL)
+            /*condicao para verificar se so ha um elemento*/
+            if (aux->prox == aux)
             {
-                printf("O numero nao esta na lista.");
-                return N;
+                l->ini = NULL;
+                l->fim = NULL;
+
+            }else{
+                aux->ant->prox = aux->prox;
+                aux->prox->ant = aux->ant;
+                    
+                if (aux == l->ini)
+                {
+                    l->ini = aux->prox;
+                }
+                if (aux == l-> fim)
+                {
+                    l->fim = aux->ant;
+                }
             }
+            
+            printf("O numero %d foi excluido com sucesso.", aux->val);
+            free(aux);
+            
+            l->tam--;
+            return l;
         }
 
-    if(p->prox!=NULL){
-        p->prox->ant = p->ant;
-    }
-    p->ant->prox =p ->prox;
-    free(p);
-    l->tam--;
-    return N;
+        aux= aux->prox;
+
+    } while (aux!=l->ini);
+
+    printf("O numero %d nao foi encontrado.\n", remover);
+    return l; /* nao achou o elemento */
+
 }
 
-no* busca(no* N, int num){
-    no* p;
-    for (p=N; p!=NULL; p=p->prox)
-    if (p->val == num){
-        printf("Numero = %d\n", p->val);
-        return p;
+void busca(lista* l){
+    no* aux = l->ini;
+    int buscar;
+    int posicao = 1;
+    int achou = 0;
+
+    if (l->ini==NULL)
+    {
+        printf("\nA lista esta vazia.\n");
+        return;
     }
-    return NULL; /* nao achou o elemento */
+
+    printf("\nInsira o numero que deseja encontrar: \n");
+    scanf("%d", &buscar);
+    getchar();
+
+    do
+    {
+        /*condicao para verificar se o numero foi encontrado*/
+        if (aux->val == buscar)
+        {
+            printf("Numero: %d\nPosicao: %d\n", aux->val, posicao);
+            achou = 1;
+            break;
+        }
+
+        aux= aux->prox;
+        posicao++;
+
+    } while (aux!=l->ini);
+
+    if (!achou)
+    {
+        printf("O numero %d nao foi encontrado.\n", buscar);
+    }
+
 }
 
-void exibir(no* N, lista* l){
-    no* p; /* variavel auxiliar para percorrer a lista */
-    printf("Primeiro Numero = %p\n", l->ini);
-    printf("Ultimo Numero = %p\n", l->fim);
+void exibir(lista* l){
+    no* aux = l->ini;
+    int posicao = 1;
+
+    if (l->ini==NULL)
+    {
+        printf("\nA lista esta vazia.\n");
+        return;
+    }
+
+    printf("\n|--------------------------|\n");
+    printf("|--| ELEMENTOS DA LISTA |--|\n");
+    printf("|--------------------------|\n");
+
+    do
+    {
+        printf("Numero: %d, Posicao: %d\n", aux->val, posicao);
+        aux= aux->prox;
+        posicao++;
+
+    } while (aux!=l->ini);
     
-    for (p = N; p != NULL; p = p->prox){
-        printf("Numero = %d\n", p->val);
-        printf("Numero anterior = %d\n, end: %p\n", p->ant->val, p->prox);
-        printf("Numero proximo= %d\n, end: %p\n", p->prox->val, p->prox);
-        //printf("endMemoria %d\n",p->prox );
-        printf("-----------Fim----------\n");
-    }
+    printf("\n|--------------------------|\n");
+    printf("Quantidade de elementos: %d.\n", l->tam);
+    printf("|-----------fim------------|\n");
 }
 
 void printMenu(){
-    printf("\n Menu de opcoes.\n");
+    printf("\nMenu de opcoes.\n");
     printf("1. Inserir um elemento no Inicio\n");
     printf("2. Inserir um elemento no Fim\n");
     printf("3. Remover um elemento\n");
     printf("4. Buscar um elemento\n");
-    printf("5. Remover um elemento\n");
+    printf("5. Exibir elementos da lista\n");
     printf("6. Encerrar o programa\n");
 }
 
 int main(){
-    no* n;
-    lista* l;
-    int num, opcoes, tam;
-    cria_lista(l, tam);
-    n = inicializa();
+    
+    lista* l = cria_lista();
+    int num, opcoes;
+
     do//Do while para fazer o menu interativo
     {
         printMenu();
         scanf("%d", &opcoes);
         getchar();
+
         switch (opcoes)
         {
-        case 1:
-            printf("\nInsira um numero: \n");
-            scanf("%d", &num);
-            getchar();
-            inserir_inicio(n, l, num, tam);
-            break;
-        case 2:
-            printf("\nInsira um numero: \n");
-            scanf("%d", &num);
-            getchar();
-            inserir_fim(n, l, num, tam);
-            break;
-        case 3:
-            printf("\nInsira um numero: \n");
-            scanf("%d", &num);
-            getchar();
-            remover(n, l, num, tam);
-            break;
-        case 4:
-            printf("\nInsira um numero: \n");
-            scanf("%d", &num);
-            getchar();
-            busca(n, num);
-            break;
-        case 5:
-            exibir(n, l);
-            break;
-        case 6:
-            break;
-        default:
-            printf("Insira uma opcao valida!");
-            break;
+            case 1:
+                printf("\nInsira um numero: \n");
+                scanf("%d", &num);
+                getchar();
+                inserir_inicio(l, num);
+                break;
+            case 2:
+                printf("\nInsira um numero: \n");
+                scanf("%d", &num);
+                getchar();
+                inserir_fim(l, num);
+                break;
+            case 3:
+                remover(l);
+                break;
+            case 4:
+                busca(l);
+                break;
+            case 5:
+                exibir(l);
+                break;
+            case 6:
+                break;
+            default:
+                printf("Insira uma opcao valida!");
+                break;
         }
     } while (opcoes!=6);
 
